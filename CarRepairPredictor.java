c2be8e830645c6c4931c0d9895b8f2b78c4c682a
Nateram <353995657@educ.dpcdsb.org>
2025-01-15 01:37:30 -0500

    CarRepairPredictorDay2

--- CarRepairPredictor.java
+++ CarRepairPredictor.java
@@ -54,20 +54,43 @@ class Cities {
 
 
 
-
+//this class is responsible for organizing all of the common car issues and costs
 public class CarRepairPredictor {
     private static carDiagnostic[] carDiagnostic = {
         new carDiagnostic("flat tire",40),
         new carDiagnostic("oil change",60),
         new carDiagnostic("AC issues",500),
-        new carDiagnostic("battery replacement",150)
+        new carDiagnostic("battery replacement",150),
+        new carDiagnostic("Brake replacement", 200),
+        new carDiagnostic("Alternator repair", 250),
+        new carDiagnostic("Transmission repair", 2000),
+        new carDiagnostic("Engine repair", 3500),
+        new carDiagnostic("Wheel alignment", 100),
+        new carDiagnostic("Exhaust repair", 150)
 
 
     };
 
+//in the main method is where the code will be implemented for the user to be able to interact with the program
+//and for the program to compute the nesscessary calulations
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-   
+        // Step 1: User enters car issue
+        System.out.println("Enter a number (1-10) corresponding to your car issue:");
+        for (int i = 0; i < carDiagnostic.length; i++) {
+            System.out.println((i + 1) + ". " + carDiagnostic[i].getDescription());
+        }
+        int carIssueChoice = scanner.nextInt();
 
+        if (carIssueChoice < 1 || carIssueChoice > 10) {
+            System.out.println("ERROR: Invalid choice. Please try again. ");
+            return;
+            
+        }
+        
+    }
+    
 
 }
 
